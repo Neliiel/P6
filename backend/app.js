156fcbe21a2ee79f25c3b498b0efb2d4fbafff0a
Nellie <n.miannay@orange.fr>
2023-02-23 13:21:41 +0100

    Mise en place de mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect('mongodb+srv://Neliiel:Tonic0690@cluster0.uiohzjr.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     console.log('Requête reçue');
@@ -17,8 +26,12 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res) => {
+app.use((req, res, next) => {
     console.log('Réponse envoyé avec succès !');
+    next();
 });
 
+
+
 module.exports = app;
+
