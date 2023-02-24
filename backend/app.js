725e0f4f70392af8c25bf55e5a4b8a05b9b075ec
Nellie <n.miannay@orange.fr>
2023-02-24 19:45:54 +0100

    Mise en place de Multer

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,8 @@ app.use('/api/sauces', saucesRoutes);
 
 app.use('/api/auth', userRoutes);
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 
 module.exports = app;
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const saucesCtrl = require('../controllers/sauces');
 
-router.post('/', auth, saucesCtrl.createThing);
-router.put('/:id', auth, saucesCtrl.modifyThing);
+router.post('/', auth, multer, saucesCtrl.createThing);
+router.put('/:id', auth, multer, saucesCtrl.modifyThing);
 router.delete('/:id', auth, saucesCtrl.deleteThing);
 router.get('/:id', auth, saucesCtrl.getOneThing);
 router.get('/', auth, saucesCtrl.getAllThings);
