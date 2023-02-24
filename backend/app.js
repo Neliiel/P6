b4c678e69d7e1a3166b9904c0645fa774bcc1714
Nellie <n.miannay@orange.fr>
2023-02-24 16:20:10 +0100

    Création route POST sauces

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,18 @@ mongoose.connect('mongodb+srv://Neliiel:Tonic0690@cluster0.uiohzjr.mongodb.net/?
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+});
+
+app.post('/api/sauces', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({ message: 'Objet créé!' });
 });
 
 app.get('/api/sauces', (req, res, next) => {
